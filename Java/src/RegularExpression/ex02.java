97d116870cc21ce906f9cee15212fa7db17a2f34
studyeonju <byj7140@naver.com>
2022-08-10 15:27:40 +0900

    pattern

--- Java/src/RegularExpression/ex02.java
+++ Java/src/RegularExpression/ex02.java
@@ -1,16 +1,17 @@
 package RegularExpression;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ex02 {
 	public static void main(String[] args) {
 		
-		Pattern pattern = Pattern.compile("(02|010)-\\d{3,4}-\\d{4}");// compile : 정규 표현식을 패턴으로 컴파일
-		String str = "010-0334-3214";
+		String regex = "(02|010)-\\d{3,4}-\\d{4}";
 		
-		Matcher matcher = pattern.matcher(str);
-		System.out.println(matcher.find());// find : 문자열에서 패턴과 일치하는 부분이 있는지 확인
+		Pattern pattern = Pattern.compile(regex);
+		// compile : 정규 표현식을 패턴으로 컴파일 
+		// String값으로 들어온 정규식을 Pattern 객체로 바꿔줍니다.
 		
+		System.out.println(pattern.pattern());
+		//pattern 객체가 갖고 있는 정규식 pattern을 반환한는 method
 	}
 }
